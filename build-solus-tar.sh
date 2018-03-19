0480ad40486b25814552f2ee5734823de1559743
tommy <tommy.shem1@googlemail.com>
2018-03-19 19:53:51 +0000

    add bash script to make solus tar image

---
+++ build-solus-tar.sh
@@ -0,0 +1,13 @@
+mkdir -p /tmp/solus
+sudo eopkg ar Solus https://packages.solus-project.com/shannon/eopkg-index.xml.xz -D /tmp/solus
+sudo eopkg update-repo
+sudo eopkg install baselayout -D /tmp/solus --ignore-safety
+sudo eopkg install -c system.base -D /tmp/solus
+#clean out cache to make smaller
+sudo eopkg dc -D /tmp/solus
+# make tar file
+sudo tar -cvpJf /tmp/solus-base-image.tar.xz -C /tmp/solus/ .
+# move to new location
+# sudo mv /tmp/solus-base-image.tar.xz /location/you/want/to/keep/it
+#clean up
+sudo rm /tmp/solus/ -rf
