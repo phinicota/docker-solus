7109f78693952995bb1024b8903a5de24db8d925
tommy <tommy.shem1@googlemail.com>
2018-03-22 00:24:20 +0000

    updated

--- build-solus-tar.sh
+++ build-solus-tar.sh
@@ -1,6 +1,6 @@
 mkdir -p /tmp/solus
 sudo eopkg ar Solus https://packages.solus-project.com/shannon/eopkg-index.xml.xz -D /tmp/solus
-sudo eopkg update-repo
+# sudo eopkg update-repo -D /tmp/solus
 sudo eopkg install baselayout -D /tmp/solus --ignore-safety
 sudo eopkg install -c system.base -D /tmp/solus
 #clean out cache to make smaller
@@ -10,4 +10,4 @@ sudo tar -cvpJf /tmp/solus-base-image.tar.xz -C /tmp/solus/ .
 # move to new location
 # sudo mv /tmp/solus-base-image.tar.xz /location/you/want/to/keep/it
 #clean up
-sudo rm /tmp/solus/ -rf
+#sudo rm /tmp/solus/ -rf
