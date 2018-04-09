dc200fb6377d92031ed0914984c189018514e05b
tommy <tommy.shem1@googlemail.com>
2018-04-09 06:37:46 +0100

    reove files not used

--- pisi/run.sh
+++
@@ -1,7 +0,0 @@
-service dbus start
-eopkg ar solus https://packages.solus-project.com/shannon/eopkg-index.xml.xz 
-eopkg install --ignore-safety pisi
-eopkg ar solus https://packages.solus-project.com/shannon/eopkg-index.xml.xz -D /home
-eopkg install --ignore-safety pisi -D /home
-eopkg install --ignore-safety --ignore-dependency -y  baselayout -D /home
-eopkg install -c  -y --ignore-safety system.base -D /home 
